40318180e1b7c1f00b2e978ebafcc7f3b2e2f85e
BeeermejoDEV <beeermejodev@gmail.com>
2025-10-14 10:44:12 +0200

    cambiar ejercicio

--- ut2Act11Pacheco..js
+++ ut2Act11Pacheco..js
@@ -1,19 +1,4 @@
 //Crear un programa que controle los turnos de la partida de ajedrez
-function iniciarPartida(){
-    let turnoActual = 1;
-    const turnosMaximos = 10;
-    while (turnoActual <= turnosMaximos) {
-        if(esTurnoBlancas(turnoActual)) {
-            console.log(`Turno ${turnoActual} -> Mueven las ♟ negras`);
-        }else{
-            console.log(`Turno ${turnoActual} -> Mueven las ♙ blancas`);
-        }
-        turnoActual++;
-    }
-    console.log("Se alcanzó el número máximo de turnos; Partida finalizada");
-}
 
-function esTurnoBlancas(turno){
-    return turno % 2 !== 0;
-}
+//Borrar codigo que subio pacheco (clono y me envio su archivo)
 
